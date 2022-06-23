860aca4750fa7a4184c5e053c781621de63e93c1
rokas <rokasra@gmail.com>
2022-06-23 16:46:01 +0300

    hardCoded

--- js/caoGet.js
+++ js/caoGet.js
@@ -1,12 +1,12 @@
-// const baseUrl = "https://zany-skitter-caper.glitch.me/experiences";
-// const cardsContEl = document.querySelector(".right");
+// const baseUrl = 'https://zany-skitter-caper.glitch.me/experiences';
+// const cardsContEl = document.querySelector('.right');
 
 // // 2. Naudojant JS, pasifetch'inti experience ir skills sritis ir jas atvaizduoti (su createElement/append) vietoj esamo HTML kodo. Čia bus du atskiri fetch'ai į https://zany-skitter-caper.glitch.me/experiences ir https://zany-skitter-caper.glitch.me/skills.
 
 // async function getPosts() {
 //   const resp = await fetch(baseUrl);
 //   const posts = await resp.json();
-//   console.log("posts ===", posts);
+//   console.log('posts ===', posts);
 
 //   renderCards(posts, cardsContEl);
 //   if (posts.success === true) {
@@ -16,15 +16,15 @@
 // getPosts();
 
 // function renderCards(cardsArr, dest) {
-//   dest.innerHTML = "";
+//   dest.innerHTML = '';
 //   cardsArr.forEach((cObj) => {
 //     dest.append(renderCard(cObj));
 //   });
 // }
 
 // function renderCard(cardObj) {
-//   const divEl = document.createElement("div");
-//   divEl.className = "cv-data";
+//   const divEl = document.createElement('div');
+//   divEl.className = 'cv-data';
 //   divEl.innerHTML = `<div class="dates">
 //   <p class="worktime"><b>${cardObj.startYear} - ${cardObj.finishYear}</b></p>
 //   <p class="workplace">${cardObj.companyName}</p>
